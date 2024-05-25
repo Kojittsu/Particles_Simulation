bffa4bab045af8aae9c968d4557b6d49dae1bae7
lolo <boudetlogan1@gmail.com>
2024-05-25 11:16:14 +0200

    removing gravity parameter from main function

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 
 int main() {
     // Parameters
-    const double gravite = -9.81;  // Gravity Acceleration (m/s^2)
     const double deltaTime = 0.01; // Time step of simulation (s)
 
     // Particles initialization
@@ -11,12 +10,10 @@ int main() {
 
     std::array<double, 2> position {0.0, 10.0};
     std::array<double, 2> velocity {0.0, 0.0};
-    std::array<double, 2> acceleration {0.0, gravite};
-
-
+    std::array<double, 2> acceleration {0.0, -9.81};
     Particle particle_1(position, velocity, acceleration);
 
-    particles.emplace_back(particle_1); // Particle at 10m height with zero initial velocity
+    particles.emplace_back(particle_1);
 
     // Create and run universe
     Universe universe(particles, deltaTime);
