cd57eaa5d9c8cf34e7f8f1ff6898086500c2be3b
lolo <boudetlogan1@gmail.com>
2024-05-28 18:17:30 +0200

    fusion of main file and SFML_example file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <array>
 #include <nlohmann/json.hpp>
+#include <SFML/Graphics.hpp>
+
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "box.h"
 #include "particle.h"
 #include "universe.h"
 
@@ -15,6 +21,25 @@ struct Config {
     int step_numbers;
     double coefficientRestitution;
 };
+
+// Structure to store space and time coordinates
+struct Coordinate {
+    double time;
+    double x;
+    double y;
+};
+
+Coordinate coord_To_SFML_Coord(const Coordinate &coordinate, const Box &box, int window_length, int window_height){
+    Coordinate SFML_coord;
+    SFML_coord.time = coordinate.time;
+    SFML_coord.x = (coordinate.x * window_length / box.getLength());
+    SFML_coord.y = window_height - ((coordinate.y) * window_height / box.getHeight());
+
+    // Debbuging (temp)
+    // std::cout << "X: " << coordinate.x << ", Y: " << coordinate.y << " --> " << "X: " << SFML_coord.x << ", Y: " << SFML_coord.y << std::endl;
+
+    return SFML_coord;
+}
 
 bool readConfig(const std::string& filename, Config& config) {
     std::ifstream file(filename);
@@ -56,6 +81,65 @@ int main() {
     // Create and run universe
     Universe universe(config.particles, config.box, config.coefficientRestitution, config.delta_time);
     universe.run(config.step_numbers, "data.csv");
+
+    // Open file
+    std::ifstream file("data.csv");
+    if (!file.is_open()) {
+        return -1;
+    }
+
+    // Vecteur pour stocker les coordonnées
+    std::vector<Coordinate> coordinates;
+    std::string line;
+
+    // Read data in file
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        Coordinate coord;
+        char comma;
+        ss >> coord.time >> comma >> coord.x >> comma >> coord.y;
+        coordinates.push_back(coord);
+    }
+
+    // Close file
+    file.close();
+
+    // Create SFML windows
+    int window_length = 1920;
+    int window_height = 1080;
+    sf::RenderWindow window(sf::VideoMode(window_length, window_height), "Particle Movement", sf::Style::Fullscreen);
+
+    // Create box
+    Box box(0.0, 10.0, 0.0, 10.0);
+
+    // Create particle
+    sf::CircleShape particle(10);
+    particle.setFillColor(sf::Color::Red);
+
+    // Index pour suivre la position actuelle de la particule
+    std::size_t index = 0;
+    sf::Clock clock;
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        // Mise à jour de la position de la particule en fonction du temps écoulé
+        if (index < coordinates.size() && clock.getElapsedTime().asSeconds() >= coordinates[index].time) {
+            Coordinate SFML_coord = coord_To_SFML_Coord(coordinates[index], box, window_length, window_height);
+            particle.setPosition(SFML_coord.x, SFML_coord.y);
+            index++;
+        }
+
+        window.clear();
+        window.draw(particle);
+        window.display();
+    }
 
     return 0;
 }
--- run.sh
+++ run.sh
@@ -2,15 +2,12 @@
 
 echo "Script begin"
 
-g++ -o compute_simu main.cpp universe.cpp particle.cpp box.cpp
-./compute_simu
-g++ -o watch_simu SFML_example.cpp box.cpp -lsfml-graphics -lsfml-window -lsfml-system
-./watch_simu
+g++ -o executable main.cpp universe.cpp particle.cpp box.cpp -lsfml-graphics -lsfml-window -lsfml-system
+./executable
 
 echo "CleanUp"
 
-rm compute_simu
+rm executable
 rm data.csv
-rm watch_simu
 
 echo "Script end"
