81b6505de6f6b643dd253eb3df8c0a9094b7f330
lolo <boudetlogan1@gmail.com>
2024-06-02 17:22:22 +0200

    moving readParticlesMovements to SFML_functions code

--- include/SFML_functions.h
+++ include/SFML_functions.h
@@ -3,6 +3,10 @@
 
 #include <SFML/Graphics.hpp>
 #include <array>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <iostream>
 #include "box.h"
 
 // Structure to store space and time coordinates
@@ -18,6 +22,8 @@ struct Coordinate {
 std::array<double, 2> coord_To_SFML_Coord(const double x, const double y, const Box &box, int window_length, int window_height);
 
 sf::VertexArray compute_border(int window_length, int window_height);
+
+std::vector<std::vector<Coordinate>> readParticlesMovements(const std::string& filename);
 
 void display_universe_SFML(std::vector<std::vector<Coordinate>> particleMovements, Box box, std::vector<double> particlesRadius, int scaleFactorPixels);
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,5 @@
-#include <SFML/Graphics.hpp>
-
-#include <sstream>
-
 #include "universe.h"
 #include "SFML_functions.h"
-
-std::vector<std::vector<Coordinate>> readParticlesMovements(const std::string& filename) {
-    std::ifstream file(filename);
-    std::string line;
-    std::map<int, std::vector<Coordinate>> particleMap;
-
-    while (std::getline(file, line)) {
-        std::istringstream iss(line);
-        std::string token;
-        std::vector<std::string> tokens;
-
-        while (std::getline(iss, token, ',')) {
-            tokens.push_back(token);
-        }
-
-        if (tokens.size() != 4) {
-            std::cerr << "Error parsing line: " << line << std::endl;
-            continue; // skip invalid lines
-        }
-
-        double time = std::stod(tokens[0]);
-        int particleNumber = std::stoi(tokens[1]);
-        double x = std::stod(tokens[2]);
-        double y = std::stod(tokens[3]);
-
-        Coordinate coord(time, x, y);
-        particleMap[particleNumber].push_back(coord);
-    }
-
-    std::vector<std::vector<Coordinate>> particleMovements;
-    for (auto& pair : particleMap) {
-        particleMovements.push_back(std::move(pair.second));
-    }
-
-    return particleMovements;
-}
 
 int main() {
     // Read configuration
