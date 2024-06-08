c13dea0e0d8ac45957a1a5cc8fd6803d4e73e5f8
lolo <boudetlogan1@gmail.com>
2024-06-08 11:28:30 +0200

    minor change

--- src/array.cpp
+++ src/array.cpp
@@ -1,28 +1,26 @@
-// src/array.cpp
-
 #include "array.h"
 
-// Implémentation de la surcharge de l'opérateur +
+// Implementation of the + operator overload
 std::array<double, 2> operator+(const std::array<double, 2>& lhs, const std::array<double, 2>& rhs) {
     return {lhs[0] + rhs[0], lhs[1] + rhs[1]};
 }
 
-// Implémentation de la surcharge de l'opérateur *
+// Implementation of the * operator overload
 std::array<double, 2> operator*(const std::array<double, 2>& lhs, const std::array<double, 2>& rhs) {
     return {lhs[0] * rhs[0], lhs[1] * rhs[1]};
 }
 
-// Implémentation de la surcharge de l'opérateur * pour un scalaire à droite
+// Implementation of the * operator overload for a right-hand scalar
 std::array<double, 2> operator*(const std::array<double, 2>& lhs, double scalar) {
     return {lhs[0] * scalar, lhs[1] * scalar};
 }
 
-// Implémentation de la surcharge de l'opérateur * pour un scalaire à gauche
+// Implementation of the * operator overload for a left-hand scalar
 std::array<double, 2> operator*(double scalar, const std::array<double, 2>& rhs) {
     return {scalar * rhs[0], scalar * rhs[1]};
 }
 
-// Implémentation de la surcharge de l'opérateur +=
+// Implementation of the += operator overload
 std::array<double, 2>& operator+=(std::array<double, 2>& lhs, const std::array<double, 2>& rhs) {
     lhs[0] += rhs[0];
     lhs[1] += rhs[1];
